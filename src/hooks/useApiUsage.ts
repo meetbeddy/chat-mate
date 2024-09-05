30b8c7c99841c8b2a9f125340f274228ea86788f
Obed Okpala <meetbeddy@gmail.com>
2024-09-05 21:27:10 +0100

    feat: refactor Api useage hook

--- src/hooks/useApiUsage.ts
+++ src/hooks/useApiUsage.ts
@@ -1,14 +1,26 @@
 import { useCallback } from "react";
 
+const getApiBaseUrl = () => {
+	const env = import.meta.env.MODE;
+
+	switch (env) {
+		case "development":
+			return import.meta.env.VITE_API_BASE_URL_DEV;
+		case "production":
+			return import.meta.env.VITE_API_BASE_URL_PROD;
+		default:
+			return import.meta.env.VITE_API_BASE_URL_DEV;
+	}
+};
+
+const API_BASE_URL = getApiBaseUrl();
+
 export const useApiUsage = () => {
 	const checkApiUsage = useCallback(async (userId: string) => {
-		const response = await fetch(
-			`http://localhost:5000/api/usage?userId=${userId}`,
-			{
-				method: "GET",
-				headers: { "Content-Type": "application/json" },
-			}
-		);
+		const response = await fetch(`${API_BASE_URL}/api/usage?userId=${userId}`, {
+			method: "GET",
+			headers: { "Content-Type": "application/json" },
+		});
 		const data = await response.json();
 		if (data.apiUsageCount >= data.apiLimit) {
 			throw new Error("API usage limit exceeded");
@@ -16,7 +28,7 @@ export const useApiUsage = () => {
 	}, []);
 
 	const updateApiUsage = useCallback(async (userId: string) => {
-		await fetch(`http://localhost:5000/api/usage?userId=${userId}`, {
+		await fetch(`${API_BASE_URL}/api/usage?userId=${userId}`, {
 			method: "POST",
 			headers: { "Content-Type": "application/json" },
 		});
