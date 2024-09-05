013b990fac9be8b964d1e2efffda6c4380837a93
Obed Okpala <meetbeddy@gmail.com>
2024-09-05 21:57:36 +0100

    add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import { RouterProvider } from 'react-router-dom'
-import './App.css'
-import { router } from './routes/Router'
+import React, { useEffect } from 'react';
+import { RouterProvider, useLocation } from 'react-router-dom';
+import './App.css';
+import { router } from './routes/Router';
+import ReactGA from 'react-ga4';
 
-function App() {
+const App: React.FC = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.initialize('G-1B4PL71XRD');
+    ReactGA.send('pageview');
+  }, []);
+
+  useEffect(() => {
+
+    ReactGA.send({ hitType: 'pageview', page: location.pathname });
+  }, [location]);
 
   return (
-    <>
-      <RouterProvider router={router} ></RouterProvider>
-    </>
-  )
-}
+    <RouterProvider router={router} />
+  );
+};
 
-export default App
+export default App;
