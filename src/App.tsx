d18da124aed348928687ae230ed9c302bf777c50
Obed Okpala <meetbeddy@gmail.com>
2024-09-06 09:52:38 +0100

    fix issue with use loaction issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import React, { useEffect } from 'react';
-import { RouterProvider, useLocation } from 'react-router-dom';
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { router } from './routes/Router';
-import ReactGA from 'react-ga4';
+
 
 const App: React.FC = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    ReactGA.initialize('G-1B4PL71XRD');
-    ReactGA.send('pageview');
-  }, []);
-
-  useEffect(() => {
-
-    ReactGA.send({ hitType: 'pageview', page: location.pathname });
-  }, [location]);
 
   return (
     <RouterProvider router={router} />
--- src/layouts/Index.tsx
+++ src/layouts/Index.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import TopNav from './TopNav';
 import Footer from './Footer';
+import ReactGA from 'react-ga4';
 
 const Layout: React.FC = () => {
+
+    const location = useLocation();
+
+    useEffect(() => {
+        ReactGA.initialize('G-1B4PL71XRD');
+        ReactGA.send('pageview');
+    }, []);
+
+    useEffect(() => {
+
+        ReactGA.send({ hitType: 'pageview', page: location.pathname });
+    }, [location]);
+
     return (
         <div className="min-h-screen flex flex-col bg-gradient-to-b from-lightGray to-lightBlue scrollbar-hide">
             <TopNav />
